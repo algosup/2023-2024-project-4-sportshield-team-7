56b089fb317882b837668a320fe83867ce5381ea
Léo CHARTIER <leo.chartier@algosup.com>
2024-03-12 23:36:41 +0100

    Add electromagnet experiment

--- src/experiments/experiments.ino
+++ src/experiments/experiments.ino
@@ -1,16 +1,29 @@
 #define BUZZER_PIN D2
+#define ELECTROMAGNET_PIN D3
 
 void setup() {
+  Serial.begin(115200);
   // Buzz to inform that the program has started
   pinMode(BUZZER_PIN, OUTPUT);
   delay(10);
   digitalWrite(BUZZER_PIN, HIGH);
   delay(1000);
   digitalWrite(BUZZER_PIN, LOW);
-  delay(5000);
+  delay(2000);
+
+  // Test the electromagnet
+  pinMode(ELECTROMAGNET_PIN, OUTPUT);
+  delay(10);
+  digitalWrite(ELECTROMAGNET_PIN, HIGH);
+  delay(1000);
+  digitalWrite(ELECTROMAGNET_PIN, LOW);
+  // With no surprise, the electromagnet did not activate
+  // This is because the board is not powerful enough to power
+  // the electromagnet directly (<2mA vs 500mA).
+  // We must contact the client to confirm the need of a relay.
 }
 
-void loop() {
+void loop_buzzer() {
   // Hard-stop, really sounds like a burst
   for (size_t i = 0; i < 16; i++) {
     digitalWrite(BUZZER_PIN, HIGH);
@@ -28,4 +41,8 @@ void loop() {
     delay(25);
   }
   delay(1000);
+}
+
+void loop() {
+
 }
