e0c4f6118b9506b38cbaa8115ff94fb33b7b29e9
Léo CHARTIER <leo.chartier@algosup.com>
2024-03-14 09:01:07 +0100

    Add LED experiment

--- src/experiments/experiments.ino
+++ src/experiments/experiments.ino
@@ -1,7 +1,16 @@
 #define BUZZER_PIN D2
 #define ELECTROMAGNET_PIN D3
 
+int i = 0;
+
 void setup() {
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_BLUE, HIGH);
+  digitalWrite(LED_GREEN, HIGH);
+
   Serial.begin(115200);
   // Buzz to inform that the program has started
   pinMode(BUZZER_PIN, OUTPUT);
@@ -44,5 +53,12 @@ void loop_buzzer() {
 }
 
 void loop() {
-
+  const int limit = 1 << 8;
+  for (int i = 0; i < limit; i++) {
+    digitalWrite(LED_RED, i & 1 ? HIGH : LOW);
+    digitalWrite(LED_BLUE, i & 2 ? HIGH : LOW);
+    digitalWrite(LED_GREEN, i & 4 ? HIGH : LOW);
+    digitalWrite(BUZZER_PIN, i & 1 ? HIGH : LOW);
+    delay(100);
+  }
 }
