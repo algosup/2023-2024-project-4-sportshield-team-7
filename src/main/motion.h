77f0ebd012939dd62ba9858ff11dba184bf624c7
Léo CHARTIER <leo.chartier@algosup.com>
2024-03-19 12:00:47 +0100

    Add gyroscope reading
    With conversion to angular velocity

--- src/main/motion.h
+++ src/main/motion.h
@@ -5,10 +5,13 @@ enum Level { off, low, high };
 // The measurement device
 extern LSM6DS3 imu;
 
-// Threshold of significant in G's
+// Threshold of significance in G's
 const float ACCELERATION_THRESHOLD_LOW = 1;
 const float ACCELERATION_THRESHOLD_HIGH = 2.5;
+const float ANGULAR_VELOCITY_THRESHOLD_LOW = 15;
+const float ANGULAR_VELOCITY_THRESHOLD_HIGH = 30;
 
 void setupMotion(void);
 float getAcceleration(void);
+float getAngularVelocity(void);
 Level getMotionLevel(void);
